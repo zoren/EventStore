d2f64e0b6f24a4a7d3c2c4715295ed396df117e0
condron <chris@condron.net>
2015-03-31 00:05:13 -0400

    First Get Test

--- src/EventStore.Core.Tests/Http/PersistentSubscription/getting.cs
+++ src/EventStore.Core.Tests/Http/PersistentSubscription/getting.cs
@@ -4,7 +4,10 @@ using System.Text.RegularExpressions;
 using EventStore.Core.Tests.Http.Users.users;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Threading;
+using Newtonsoft.Json.Linq;
 using HttpStatusCode = System.Net.HttpStatusCode;
+using EventStore.Transport.Http;
 
 // ReSharper disable InconsistentNaming
 
@@ -14,7 +17,10 @@ namespace EventStore.Core.Tests.Http.PersistentSubscription
     class with_subscription_having_events : with_admin_user
     {
         protected List<object> Events;
+        protected string SubscriptionPath;
         protected string GroupName;
+        
+        
 
         protected override void Given()
         {
@@ -25,14 +31,15 @@ namespace EventStore.Core.Tests.Http.PersistentSubscription
                 new {EventId = Guid.NewGuid(), EventType = "event-type", Data = new {C = "3"}},
                 new {EventId = Guid.NewGuid(), EventType = "event-type", Data = new {D = "4"}}
             };
+
             var response = MakeArrayEventsPost(
                          TestStream,
                          Events,
                          _admin);
             Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
             GroupName = Guid.NewGuid().ToString();
-            response = MakeJsonPut(
-                "/subscriptions/" + TestStream.Substring(8) + "/" + GroupName,
+            SubscriptionPath = string.Format("/subscriptions/{0}/{1}",TestStream.Substring(8),GroupName);
+            response = MakeJsonPut( SubscriptionPath,
                 new
                 {
                     ResolveLinkTos = true
@@ -43,7 +50,7 @@ namespace EventStore.Core.Tests.Http.PersistentSubscription
 
         protected override void When()
         {
-            
+
         }
     }
 
@@ -51,6 +58,7 @@ namespace EventStore.Core.Tests.Http.PersistentSubscription
     {
         protected override void Given()
         {
+
         }
 
         protected override void When()
@@ -60,32 +68,35 @@ namespace EventStore.Core.Tests.Http.PersistentSubscription
         [Test]
         public void returns_0_messages()
         {
+           
             //todo: implement test
             //not proven to pass or fail yet
-            Assert.Inconclusive("Not Implemented");
+             Assert.Inconclusive("Not Implemented");
         }
     }
 
     class when_getting_messages_from_a_subscription_with_n_messags_return_n_messages : with_subscription_having_events
     {
-        
+        private JObject _response;
         protected override void When()
         {
-
+            _response = GetJson<JObject>(
+                                SubscriptionPath + "/messages?count=" + 4 ,
+                                ContentType.Any, //todo CLC sort out allowed content typees
+                                _admin);
         }
 
         [Test]
         public void returns_n_messages()
         {
-            //todo: implement test
-            //not proven to pass or fail yet
-            Assert.Inconclusive("Not Implemented");
+            var count = ((JArray)_response["events"]).Count;
+            Assert.AreEqual(Events.Count, count,"Expected {0} events, received {1}", Events.Count, count);
         }
     }
 
     class when_getting_messages_from_a_subscription_with_more_than_n_messags_return_n_messages : with_subscription_having_events
     {
-        
+
         protected override void Given()
         {
         }
@@ -145,7 +156,7 @@ namespace EventStore.Core.Tests.Http.PersistentSubscription
 
     class when_getting_messages_from_a_subscription_with_a_negative_count : with_subscription_having_events
     {
-    
+
         protected override void Given()
         {
         }
@@ -185,7 +196,7 @@ namespace EventStore.Core.Tests.Http.PersistentSubscription
 
     class when_getting_messages_from_a_subscription_with_count_more_than_100 : with_subscription_having_events
     {
-        
+
         protected override void Given()
         {
         }
